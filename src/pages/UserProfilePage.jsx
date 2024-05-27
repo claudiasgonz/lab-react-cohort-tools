363d6f6d0975cf6e28bd7fddbc97af9011c57961
Claudia G <claudsain@gmail.com>
2024-05-27 08:06:32 -0400

    lab done

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 function UserProfilePage() {
   const userProfile = {
-    image: "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png",
+    image:
+      "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png",
     name: "Jane Doe",
     email: "janed@example.com",
     role: "Lead Teacher",
   };
+
+  console.log(window.location.href);
 
   return (
     <div className="StudentDetailsPage bg-gray-100 py-6 px-4 border-2 border-violet-500 m-2">
@@ -32,11 +37,12 @@ function UserProfilePage() {
           </>
         )}
 
-        {/* Back button */}        
-        <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
-          Back
-        </button>
-        
+        {/* Back button */}
+        <Link to="/">
+          <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+            Back
+          </button>
+        </Link>
       </div>
     </div>
   );
